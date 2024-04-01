f5da790e82ba268d573e66e308feca205d8bd264
Palt <fakerzx98@gmail.com>
2024-04-01 10:39:02 +0300

    step 3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,7 +2,6 @@ plugins {
     id("java")
     id("se.patrikerdes.use-latest-versions") version "0.2.18"
     id("com.github.ben-manes.versions") version "0.50.0"
-    kotlin("kapt") version "1.9.23"
     application
     checkstyle
 }
@@ -18,16 +17,19 @@ application {
     mainClass.set("hexlet.code.App")
 }
 
+tasks {
+    compileJava {
+        options.release = 20
+    }
+}
+
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
-    kapt ("info.picocli:picocli-codegen:4.7.5")
-}
-
-kapt {
-    arguments {
-        arg("project", "${project.group}/${project.name}")
-    }
+    implementation("org.apache.commons:commons-lang3:3.14.0")
+    implementation("org.apache.commons:commons-collections4:4.4")
+    implementation ("info.picocli:picocli:4.7.5")
+    annotationProcessor ("info.picocli:picocli-codegen:4.7.5")
 }
 
 tasks.test {
