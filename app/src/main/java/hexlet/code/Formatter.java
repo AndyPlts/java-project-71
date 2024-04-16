3cc5de192bb2c262bc0ae5334d0f59605ec04447
Palt <fakerzx98@gmail.com>
2024-04-16 11:23:54 +0300

    step 8 fix

--- app/src/main/java/hexlet/code/Formatter.java
+++ app/src/main/java/hexlet/code/Formatter.java
@@ -9,40 +9,56 @@ public class Formatter {
         resultString.append("{");
         for (var field : diffResult) {
             if (field.get("status").equals("removed")) {
-                resultString.append("\n  ")
-                        .append("- ")
-                        .append(field.get("Key"))
-                        .append(": ")
-                        .append(field.get("old_value"));
+                correctRemoved(field, resultString);
             }
             if (field.get("status").equals("added")) {
-                resultString.append("\n  ")
-                        .append("+ ")
-                        .append(field.get("Key"))
-                        .append(": ")
-                        .append(field.get("old_value"));
+                correctAdded(field, resultString);
             }
             if (field.get("status").equals("edited")) {
-                resultString.append("\n  ")
-                        .append("- ")
-                        .append(field.get("Key"))
-                        .append(": ")
-                        .append(field.get("old_value"));
-                resultString.append("\n  ")
-                        .append("+ ")
-                        .append(field.get("Key"))
-                        .append(": ")
-                        .append(field.get("new_value"));
+                correctEdited(field, resultString);
             }
             if (field.get("status").equals("no_changes")) {
-                resultString.append("\n  ")
-                        .append("  ")
-                        .append(field.get("Key"))
-                        .append(": ")
-                        .append(field.get("old_value"));
+                correctNoChanges(field, resultString);
             }
         }
         resultString.append("\n}");
         return resultString.toString();
+    }
+
+    private static void correctRemoved(Map<String, Object> field, StringBuilder resultString) {
+        resultString.append("\n  ")
+                .append("- ")
+                .append(field.get("Key"))
+                .append(": ")
+                .append(field.get("old_value"));
+    }
+
+    private static void correctNoChanges(Map<String, Object> field, StringBuilder resultString) {
+        resultString.append("\n  ")
+                .append("  ")
+                .append(field.get("Key"))
+                .append(": ")
+                .append(field.get("old_value"));
+    }
+
+    private static void correctAdded(Map<String, Object> field, StringBuilder resultString) {
+        resultString.append("\n  ")
+                .append("+ ")
+                .append(field.get("Key"))
+                .append(": ")
+                .append(field.get("old_value"));
+    }
+
+    private static void correctEdited(Map<String, Object> field, StringBuilder resultString) {
+        resultString.append("\n  ")
+                .append("- ")
+                .append(field.get("Key"))
+                .append(": ")
+                .append(field.get("old_value"));
+        resultString.append("\n  ")
+                .append("+ ")
+                .append(field.get("Key"))
+                .append(": ")
+                .append(field.get("new_value"));
     }
 }
