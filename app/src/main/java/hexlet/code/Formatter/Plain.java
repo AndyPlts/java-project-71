91bb2423a55a91f98fc8430b40327bd3966fb700
Palt <fakerzx98@gmail.com>
2024-05-03 17:08:30 +0300

    fix

--- app/src/main/java/hexlet/code/Formatter/Plain.java
+++ app/src/main/java/hexlet/code/Formatter/Plain.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Map;
 
 public class Plain {
+    private static final String CORRECT_ADDED_STRING = "Property '%s' was added with value: %s\n";
+    private static final String CORRECT_EDITED_STRING = "Property '%s' was updated. From %s to %s\n";
     public static String getPlain(List<Map<String, Object>> diffResult) {
         StringBuilder resultString = new StringBuilder();
         for (var field : diffResult) {
@@ -12,27 +14,19 @@ public class Plain {
                     resultString.append(String.format("Property '%s' was removed\n", field.get("Key")));
                     break;
                 case "added":
-                    resultString.append(correctAdded(field));
+                    resultString.append(String.format(CORRECT_ADDED_STRING,
+                            field.get("Key"), getFormattedValue(field.get("value"))));
                     break;
                 case "edited":
-                    resultString.append(correctEdited(field));
+                    resultString.append(String.format(CORRECT_EDITED_STRING,
+                            field.get("Key"), getFormattedValue(field.get("old_value")),
+                            getFormattedValue(field.get("new_value"))));
                     break;
                 default:
             }
         }
         return resultString.toString().trim();
     }
-
-    private static String correctEdited(Map<String, Object> field) {
-        return String.format("Property '%s' was updated. From %s to %s\n",
-                field.get("Key"), getFormattedValue(field.get("old_value")), getFormattedValue(field.get("new_value")));
-    }
-
-    private static String correctAdded(Map<String, Object> field) {
-        return String.format("Property '%s' was added with value: %s\n",
-                field.get("Key"), getFormattedValue(field.get("value")));
-    }
-
     private static <T> String getFormattedValue(T value) {
         if (value instanceof String) {
             return "'" + value + "'";
